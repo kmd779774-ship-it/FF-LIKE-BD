6ad86c6d5f08f4ed527e7ad21fbf55d58405a75b
kmd779774-ship-it <kmd779774@gmail.com>
2026-09-24 01:15:58 +0600

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,3 @@
-
+const functions=require('firebase-functions');const admin=require('firebase-admin');admin.initializeApp();
+exports.likeOrder=functions.https.onCall(async(data)=>{return {status:'connect-like-api-here',uid:data.uid};});
+exports.bohudurVerify=functions.https.onRequest(async(req,res)=>{res.json({status:'implement-bohudur-callback'});});
